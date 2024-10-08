1e62a43c5b3ab017caf8cd499f7e80cf6220ee9f
Luis Ramirez <luis.ramirez6@utp.ac.pa>
2024-10-08 12:06:44 -0500

    Actualización del laboratorio con recomendaciones del profesor

---
+++ laboratorio_7/utils.js
@@ -0,0 +1,14 @@
+const Utils = (() => {
+    const generarColorAleatorio = () => {
+        const letras = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letras[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    return {
+        generarColorAleatorio,
+    };
+})();
